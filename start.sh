1f5b3b43119be17010a27dd0f6eff3ec9abedbd6
Hari Krishna <harikrishnabiyyala@gmail.com>
2025-03-14 00:37:00 +0530

    modified start script

--- start.sh
+++ start.sh
@@ -3,10 +3,9 @@
 mvn package -Dmaven.test.skip
 
 java -javaagent:src/main/resources/elastic-apm-agent-1.51.0.jar \
-#-Delastic.apm.service_name=apm-demo-application \
-#-Delastic.apm.server_urls=http://localhost:8200 \
-#-Delastic.apm.secret_token= \
-#-Delastic.apm.environment=development \
-#-Delastic.apm.application_packages=com.cosmin.tutorials.apm \
--Delastic.apm.config_file=src/main/resources/apm-config.properties \
+-Delastic.apm.service_name=apm-demo-application \
+-Delastic.apm.server_urls=http://localhost:8200 \
+-Delastic.apm.secret_token= \
+-Delastic.apm.environment=development \
+-Delastic.apm.application_packages=com.cosmin.tutorials.apm \
 -jar target/apm-0.0.1-SNAPSHOT.jar
